aaad6f04a850a1201285d93927622d4d9ef8dc12
xcj7 <xcjtushar@gmail.com>
2022-08-10 05:01:08 +0600

    .

--- React-Learning-main/reactapp/src/Shop/Detaill.js
+++ React-Learning-main/reactapp/src/Shop/Detaill.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Head from "./Head";
+import Style from "../Component/Style/product.module.css";
 export default function Details(){
     const {id} =useParams();
     const [product, setProduct] = useState([]);
@@ -15,14 +16,23 @@ export default function Details(){
         
     },[]);
     return(
-       <div>
+        <>
         <Head />
+        <div className={Style.main}>
+        <div>
+        
+        <img  src={"http://localhost:8000/shop/" + product.image} />
         <h1>Name: {product.p_name}</h1>
         <h1>price: {product.p_price}</h1>
+        <h1>price: {product.p_description}</h1>
         {/* <h1>image:    src="/shop/{product.image}" </h1> */}
         {/* <img src={product.image}/> */}
-        <img  src={"http://localhost:8000/shop/" + product.image} />
+       
         {/* C:\xampp\htdocs\laravel projects\Task_6\laravel_app\public\shop\halowa.jpg1659785496.jpg */}
        </div>
+        </div>
+        
+        </>
+      
     )
 }
--- React-Learning-main/reactapp/src/Shop/Profile_seller.js
+++ React-Learning-main/reactapp/src/Shop/Profile_seller.js
@@ -1,108 +1,102 @@
-import Style from "../Component/Style/product.module.css";
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import Style from "../Component/Style/product.module.css";
+import img from "../Component/Image/shop2.webp";
+import Head from "./Head";
 import { Link, useNavigate } from "react-router-dom";
-import { useParams } from "react-router-dom";
-// import profile from "../Image/profile.jpg";
-// import email from "../Image/email.png";
-// import phone from "../Image/phone.png";
-// import nid from "../Image/nid.png";
-// import address from "../Image/address.gif";
-import Nav from "./Head";
-export default function AProfile(){
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
-  const {id} =useParams();
+export default function AProfile(){
   const [user, setUser] = useState([]);
   const [name, setName] = useState([]);
   const [email, setEmail] = useState([]);
   const [phone, setPhone] = useState([]);
   const [nid, setNid] = useState([]);
+  const [password, setPassword] = useState([]);
   const [address, setAddress] = useState([]);
-  const [iid, setID] = useState([]);
-
-  const [loading, setLoading] = useState(true);
-useEffect(()=>{
-  setTimeout(()=>{
-    setLoading(false);
-  },1000);
-},[loading]);
-
-
-
+  const [id, setID] = useState(0);
+  const navigate = useNavigate('');
   useEffect(()=>{
 
-    axios.get(`http://127.0.0.1:8000/api/profile/${id}`).then(response =>{
-        setUser(response.data);
-       setName(response.data.name);
-       setEmail(response.data.email);
-       setPhone(response.data.phone)
-       setNid(response.data.nid);
-       setAddress(response.data.address);
-       setID(response.data.id);
-       
-    });
-    
-},[]);
+    if (localStorage.getItem('user')){
+     let obj = JSON.parse(localStorage.getItem('user'));
+     console.log(obj.userId);
+     axios.get(`http://127.0.0.1:8000/api/update/${obj.userId}`).then(resp =>{
+   
+       setUser(resp.data);
+       setName(resp.data.name);
+       setEmail(resp.data.email);
+       setPhone(resp.data.phone)
+       setNid(resp.data.nid);
+       setAddress(resp.data.address);
+       setPassword(resp.data.password);
+       setID(resp.data.id);
+      }).catch(err=>{
+       console.log(err);
+      }
+     );
 
+ }else{
+     alert("You are not logged in. Login or registration first");
+     navigate('/login');
+ }
 
+ },[]);
 
-
-
-   const onSubmitted= (e) =>{
-        e.preventDefault();
-        var obj = {name:name, email: email, phone: phone, nid: nid, address: address, id: iid};
-        axios.post("http://127.0.0.1:8000/api/update",obj)
-        .then(resp=>{
-            var token = resp.data;
-            alert(token);
-           
-        }).catch(err=>{
-            console.log(err);
-        });
-    }
+ const onSubmitted= (e) =>{
+  e.preventDefault();
+  var obj = { id:id,name:name, email: email, phone: phone, nid: nid, address: address, password: password};
+  axios.post("http://127.0.0.1:8000/api/update",obj)
+  .then(resp=>{
+      var token = resp.data;
+      alert(token);
+     
+  }).catch(err=>{
+      console.log(err);
+  });
+}
     return(
     <>
- <Nav />
+ <Head />
      <div className={Style.main}>
-          
+    
    
-      <div className={Style.product}>
+      <div  className={Style.product}>
        <h2>EDIT PROFILE</h2>
    
        <Form>
        <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>FULL NAME</Form.Label>
-        <Form.Control type="text" placeholder="Enter Name" />
-        
+       <Form.Control type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name" />
       </Form.Group>
+
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>EMAIL ADDRESS</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Label>EMAIL </Form.Label>
+        <Form.Control type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter email" />
+  
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPhone">
         <Form.Label>PHONE</Form.Label>
-        <Form.Control type="phone" placeholder="Enter phone" />
+        <Form.Control type="phone" value={phone} onChange={(e)=>setPhone(e.target.value)} placeholder="Enter Phone" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicNid">
         <Form.Label>NID</Form.Label>
-        <Form.Control type="nid" placeholder="Enter NID" />
+        <Form.Control type="nid" value={nid} onChange={(e)=>setNid(e.target.value)} placeholder="Enter Nid" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicAddress">
         <Form.Label>ADDRESS</Form.Label>
-        <Form.Control type="address" placeholder="Enter Address" />
+        <Form.Control type="text" value={address} onChange={(e)=>setAddress(e.target.value)} placeholder="Enter address" />
       </Form.Group>
-
-      <button type="submit"  onClick={onSubmitted} className="btn btn-success">
-                            Edit
-                        </button>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password" />
+      </Form.Group>
+      <input type='hidden' value={id} onChange={(e)=>setID(e.target.value)} />
+      <Button variant="success" onClick={onSubmitted} type="submit">EDIT</Button>
      </Form>
       </div>
      </div>
